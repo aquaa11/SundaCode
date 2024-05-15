9e5af5dd60cac84b175ed2ce968542cae1eb94cd
aquaa11 <158056191+aquaa11@users.noreply.github.com>
2024-05-16 00:52:48 +0700

    asd

--- pages/home/index.js
+++ pages/home/index.js
@@ -74,7 +74,7 @@ export default function Home() {
               </Link>{" "}
             </button>
           </div>
-          <div className="bg-white h-[460px] sm:bg-center md:w-1/4  bg-[url('/public/poster-pamali.jpg')] bg-cover bg-bottom"></div>
+          <div className="bg-white h-[460px] sm:bg-center md:w-1/4  bg-[url('/../../public/poster-pamali.jpg')] bg-cover bg-bottom"></div>
         </div>
         <div className="px-5 pt-6 pb-8 pb sm:py-6 sm:px-8 bg-white flex w-full max-w-screen-xl flex-col sm:flex-row mx-auto gap-5 sm:gap-10">
           <div className="bg-white w-full sm:w-1/2 h-80 sm:h-[22rem]">
